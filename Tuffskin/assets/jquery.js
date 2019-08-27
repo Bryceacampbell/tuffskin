ddd5120f5a3d9cf6c69a2566083a5217a36ee6b2
bryceacampbell <bryceacampbell7@gmail.com>
2019-08-26 20:43:11 -0700

    layout about us page, complete styling, fixed nav

---
+++ Tuffskin/assets/jquery.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('navbar-background');
+          $('.logo').addClass('shrink-logo')
+    }
+
+    else {
+          $('nav').removeClass('navbar-background');
+          $('.logo').removeClass('shrink-logo')
+    }
+})
